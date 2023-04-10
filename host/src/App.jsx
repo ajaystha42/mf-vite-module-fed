3e27ea609c89fdf7df513da89dc2a997e0ab540e
Ajay Shrestha <er.ajaystha@gmail.com>
2023-04-10 16:45:10 -0400

    webpack host setup

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -20,27 +20,3 @@ function App() {
 }
 
 export default App;
-
-// import { useState } from "react";
-// import "./App.css";
-
-// import Button from "remoteApp/Button";
-// import useStore from "remoteApp/store";
-
-// function App() {
-//   const [count, setCount] = useStore();
-
-//   return (
-//     <div className="App">
-//       <h1>Host Application</h1>
-//       <Button />
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
